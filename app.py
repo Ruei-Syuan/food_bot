0300447b38c19fad59073c0196435ac350b291bd
Ruei-Syuan <syuan111251@gmail.com>
2025-06-14 16:24:14 +0800

    20250614

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 # ============================== ENVIROMENT ====================================
-from flask import Flask, request, abort
+from flask import Flask, request #, abort
 
 from linebot.v3 import (
     WebhookHandler
 )
-from linebot.v3.exceptions import (
-    InvalidSignatureError
-)
+# from linebot.v3.exceptions import (
+    # InvalidSignatureError
+# )
 from linebot.v3.messaging import (
     Configuration,
-    ApiClient,
-    MessagingApi,
-    ReplyMessageRequest,
-    TextMessage
+    # ApiClient,
+    # MessagingApi,
+    # ReplyMessageRequest,
+    # TextMessage
 )
-from linebot.v3.webhooks import (
-    MessageEvent,
-    TextMessageContent
-)
+# from linebot.v3.webhooks import (
+#     MessageEvent,
+#     TextMessageContent
+# )
 import os
 from linebot import LineBotApi, WebhookHandler
-from linebot.models import MessageEvent, TextMessage, TextSendMessage,TextSendMessage, LocationSendMessage
+from linebot.models import TextSendMessage,TextSendMessage, LocationSendMessage
+# MessageEvent, TextMessage, 
 import json
 import sqlite3
 
